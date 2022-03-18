c2f94d2f62c678f316feb10ddd748cb4a426e5ee
Romylussone <romylussone@gmail.com>
2022-03-18 10:59:06 +0100

    little changes upcomming

--- 404.php
+++ 404.php
@@ -1,3 +1,7 @@
+<?php 
+  require_once "./config/checkConfig.php"; 
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php 
   require_once "is_connect.php"; 
-
+  require_once "./config/checkConfig.php"; 
   $date_gen = date("Y-m-d");
 ?>
 
