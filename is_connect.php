40965551c9c47176cbfd3315faa93ee84f3e6654
Romylussone <romylussone@gmail.com>
2022-03-18 11:01:11 +0100

    gestion roles somes changes

--- is_connect.php
+++ is_connect.php
@@ -1,32 +1,14 @@
 <?php
+require_once "./proccess/ajax/liste_module_app.php";
+
 session_start();
 
 if (!isset($_SESSION['user'])) {
 	header('Location: ./login.php');
 } else {
 
-
-	// Définition des modules de la sidebar
-	$modules_saisie = [
-		"saisie_controle_acte_lot" => "Contrôle Acte", "correction_acte" => "Correction Acte", "correction_reqs" => "Vigilance", "correction_acte" => "Correction Acte",
-		"initialisation_lot" => "Initialisation d'un Lot", "validation_lot" => "Validation Lot", "division_lot" => "Division Lot"
-	];
-	$modules_action_IEC = ["saisie_controle_acte_lot_auto" => " Contrôle IEC", "actioniec_controle_unitaire" => "Contrôle Unitaire"];
-	$modules_action_OEC_POPF = ["controle_oec_popf" => "Contrôle OEC-POPF"];
-	$modules_livraison = [
-		"purge_lot" => "Purge Lot", "controle_inventaire_liv" => "Contrôle Inventaire", "controle_general_liv" =>
-		"Contrôle Général", "transfert_lot" => "Transfert Lot", "split_bd" => "Split BD",
-		"livraison_extraction_stats" => "Extraction BD"
-	];
-	$modules_gestion_ecm = [
-		"gestion_users" => "Gestion utilisateur", "gestion_group_users" => "Gestion Group Util.", "gestion_db_setting" => "Gestion BD",
-		"gestion_pref_setting" => "Préférences", "gestion_impotation_setting" => "Gestion Importation", "script_gestion" => "Script de gestion"
-	];
-
 	// Définition des règles d'accès
 	$scripts_name = explode("/", $_SERVER["SCRIPT_NAME"]);
-	// var_dump($scripts_name);
-	// die();
 
 	if (isset($_SESSION['user']->list_role)) {
 		$array_fake[0] = "";
@@ -41,7 +23,8 @@ if (!isset($_SESSION['user'])) {
 			header('Location: ./404.php');
 		}
 	} else {
-		header('Location: ./404.php');
+		$_SESSION['user']->list_role = "index,404";
+		header('Location: ./index.php');
 	}
 
 	// Chargement de la couleur de session
---
+++ proccess/ajax/liste_module_app.php
@@ -0,0 +1,18 @@
+<?php
+
+// Définition des modules 
+$modules_saisie = [
+  "saisie_controle_acte_lot" => "Contrôle Acte", "correction_acte" => "Correction Acte", "correction_reqs" => "Vigilance", "correction_acte" => "Correction Acte",
+  "initialisation_lot" => "Initialisation d'un Lot", "validation_lot" => "Validation Lot", "division_lot" => "Division Lot"
+];
+$modules_action_IEC = ["saisie_controle_acte_lot_auto" => " Contrôle IEC", "actioniec_controle_unitaire" => "Contrôle Unitaire"];
+$modules_action_OEC_POPF = ["controle_oec_popf" => "Contrôle OEC-POPF"];
+$modules_livraison = [
+  "purge_lot" => "Purge Lot", "controle_inventaire_liv" => "Contrôle Inventaire", "controle_general_liv" =>
+  "Contrôle Général", "transfert_lot" => "Transfert Lot", "split_bd" => "Split BD",
+  "livraison_extraction_stats" => "Extraction BD"
+];
+$modules_gestion_ecm = [
+  "gestion_users" => "Gestion utilisateur", "gestion_group_users" => "Gestion Group Util.", "gestion_db_setting" => "Gestion BD",
+  "gestion_pref_setting" => "Préférences", "gestion_impotation_setting" => "Gestion Importation", "script_gestion" => "Script de gestion"
+];
